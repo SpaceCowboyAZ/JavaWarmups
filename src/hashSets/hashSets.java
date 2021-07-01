8c81dcb763e6cf43f4d92eb3774e2907decdd0bb
SpaceCowboyAZ <77759985+SpaceCowboyAZ@users.noreply.github.com>
2021-07-01 11:16:16 -0700

    Add files via upload

--- src/hashSets/hashSets.java
+++ src/hashSets/hashSets.java
@@ -1,10 +1,43 @@
 package hashSets;
 
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class hashSets {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		
+		HashSet h = new HashSet();
+		h.add("Fender");
+		h.add("Gibson");
+		h.add("Jackson");
+		h.add("ESP");
+		
+		System.out.println(h.contains("Squire"));
+		
+		HashSet<Integer> ints = new HashSet<Integer>();
+		ints.add(44);
+		ints.add(23);
+		ints.add(65);
+		
+		
+		
+		System.out.println(ints);
+		
+		//can't rely on order in hashsets/maps
+		Object[] o = ints.toArray();
+		System.out.println(o[0]);
+		
+		
+		// prints jibberish
+		//System.out.println(ints.toArray());
+		
+		Iterator<Integer> it = ints.iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+		
 	}
 
 }
