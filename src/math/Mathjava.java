0b426dde46cd923220ce94aa44480ca14898444c
SpaceCowboyAZ <77759985+SpaceCowboyAZ@users.noreply.github.com>
2021-07-14 21:06:26 -0700

    Add files via upload

---
+++ src/math/Mathjava.java
@@ -0,0 +1,15 @@
+package math;
+
+public class Mathjava {
+
+	public static void main(String[] args) {
+	System.out.println(Math.class);
+	System.out.println(Math.E);
+	System.out.println(Math.PI);
+	
+	System.out.println(Math.abs(12.2));
+	
+	System.out.println(Math.acos(-3.1));
+	}
+
+}
